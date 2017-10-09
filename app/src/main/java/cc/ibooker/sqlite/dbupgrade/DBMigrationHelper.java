b8aaeb4f0b4a25bd8c0ff0dc9890bcea8307a3b6
zrunker <zrunker@yahoo.com>
2017-10-09 18:57:50 +0800

    init Commit

--- app/src/main/java/cc/ibooker/sqlite/dbupgrade/DBMigrationHelper.java
+++ app/src/main/java/cc/ibooker/sqlite/dbupgrade/DBMigrationHelper.java
@@ -12,15 +12,24 @@ public class DBMigrationHelper extends AbstractMigratorHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db) {
-        // 创建t_user临时表t_user2
-        db.execSQL(SQLiteConstant.SQL_CREATE_TABLE_USER2);
-        // 复制t_user到t_user2
-        db.execSQL(SQLiteConstant.SQL_COPY_TABLE_USER_USER2);
-        // 删除t_user
-        db.execSQL(SQLiteConstant.SQL_DROP_TABLE_USER);
-        // 重命名t_user2为t_user
-        db.execSQL(SQLiteConstant.SQL_USER2_RENAME_USER);
-        // 也可以额外进行其他操作
+        db.beginTransaction();
+        try {
+            // 创建t_user临时表t_user2
+            db.execSQL(SQLiteConstant.SQL_CREATE_TABLE_USER2);
+            // 复制t_user到t_user2
+            db.execSQL(SQLiteConstant.SQL_COPY_TABLE_USER_USER2);
+            // 删除t_user
+            db.execSQL(SQLiteConstant.SQL_DROP_TABLE_USER);
+            // 重命名t_user2为t_user
+            db.execSQL(SQLiteConstant.SQL_USER2_RENAME_USER);
+            // 也可以额外进行其他操作
+            // ...
+            db.setTransactionSuccessful();
+        }catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();
+        }
     }
 
 }
